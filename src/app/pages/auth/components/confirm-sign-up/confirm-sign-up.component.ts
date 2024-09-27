aed46287ef683b2b64793fd20db62ce173ef6325
Juan Carlos Hoyos <juan.hoyos@integrait.co>
2024-09-27 15:23:00 -0500

    ✨create confirm sign up component

---
+++ src/app/pages/auth/components/confirm-sign-up/confirm-sign-up.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-sign-up',
+  templateUrl: './confirm-sign-up.component.html',
+  styleUrls: ['./confirm-sign-up.component.scss'],
+})
+export class ConfirmSignUpComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
